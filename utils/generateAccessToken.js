88d49ef75fd8ce20baa8cbee45dd2de78051703b
Yash verma <yashverma9084+github@gmail.com>
2026-06-09 21:39:18 +0530

    Generate Acess Token

---
+++ utils/generateAccessToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken")
+
+module.exports = user =>{
+    jwt.sign(
+        {
+            id:user._id,
+            role:user.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"15m"
+        }
+    )
+}
